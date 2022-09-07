b638be08aedb4d98ef2084afc06474c009d7cb2c
Angus Gaukroger <agaukroger77@gmail.com>
2022-09-08 00:36:34 +0100

    Done for today I think

--- PortfolioTake2/Controllers/FlashcardAppController.cs
+++ PortfolioTake2/Controllers/FlashcardAppController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using PortfolioTake2.Models;
 using System.Diagnostics;
 
@@ -7,6 +8,12 @@ namespace PortfolioTake2.Controllers
     public class FlashcardAppController : Controller
     {
         public IActionResult Landing()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult SetView()
         {
             return View();
         }
